b23ec6be8a6237916e358e5e839fe2e6e8b32257
Mateus De Paula <mateuspaula@ceert.org.br>
2023-10-27 11:23:13 -0300

    first commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import style from './Botao.module.scss'
+
+export function Botao(){
+    return (
+        <button className={style.botao}>
+            Botão
+        </button>
+    )
+}
+
+export default Botao;
